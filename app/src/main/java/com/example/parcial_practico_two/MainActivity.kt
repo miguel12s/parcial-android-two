705c0589609554e6f820bf20943788da1ea250bd
miguel12s <87570624+miguel12s@users.noreply.github.com>
2024-03-23 13:11:06 -0500

    commit

---
+++ app/src/main/java/com/example/parcial_practico_two/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.parcial_practico_two
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
